7face9450c991f5711cb31fd1219209dba2db5d6
hori130112 <1419084@ed.tus.ac.jp>
2019-06-22 13:29:39 +0900

    aa

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const el=document.querySelector(".a")
+console.log(el)
 function  jannkenn(a){
 if (a=='gu'){
 console.log("グー")
@@ -20,34 +22,52 @@ if(2/3<=b){console.log("チョキ")
 if(a=='gu'){
     if(b<1/3){
     console.log("負け")
+    el.innerText="負け"
+    console.log(el.innerText)
 }
     if(1/3<=b&&b<2/3){
         console.log("引き分け")
+        el.innerText="引き分け"
+        console.log(el.innerText)
     }
     if(2/3<=b){
         console.log("勝ち")
+        el.innerText="勝ち"
+        console.log(el.innerText)
     }
 }
 if (a=='tyoki'){
  if(b<1/3){
      console.log("勝ち")
+     el.innerText="勝ち"
+     console.log(el.innerText)
  }
  if(1/3<=b&&b<2/3){
      console.log("負け")
+     el.innerText="負け"
+     console.log(el.innerText)
  }
  if(2/3<=b){
      console.log("引き分け")
+     el.innerText="引き分け"
+     console.log(el.innerText)
  }
 }
 if(a=='pa'){
     if(b<1/3){
         console.log("引き分け")
+        el.innerText="引き分け"
+        console.log(el.innerText)
     }
     if(1/3<=b&&b<2/3){
         console.log("勝ち")
+        el.innerText="勝ち"
+        console.log(el.innerText)
     }
     if(2/3<=b){
         console.log("負け")
+        el.innerText="負け"
+         console.log(el.innerText)
     }
 }
 }
